49930ffbbe1d8b18f371993cd6aa6ebf569dfb18
sagarshankarsonar <42066769+sagarshankarsonar@users.noreply.github.com>
2024-10-17 17:10:17 +0530

    Update Chart.jsx

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -17,18 +17,18 @@ const Chart = ({ data:{ confirmed, deaths, recovered }, country }) => {
     },[]);
 
     const lineChart = (
-        dailyData.length
+        dailyData?.length
         ? (
         <Line 
         data={{
-            labels:dailyData.map(({ date }) => date),
+            labels:dailyData?.map(({ date }) => date),
             datasets:[{
-                data: dailyData.map(({ confirmed }) => confirmed),
+                data: dailyData?.map(({ confirmed }) => confirmed),
                 label: 'Infected',
                 borderColor:'#3333ff',
                 fill:true,
             },{
-                data: dailyData.map(({ deaths }) => deaths),
+                data: dailyData?.map(({ deaths }) => deaths),
                 label: 'Deaths',
                 borderColor:'red',
                 backgroundColor:'rgba(255,0,0,0.5)',
